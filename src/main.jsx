87a0a92c4c61112ffeec6bbf82b7f8c3513aa910
Luis-Berrio <laberrio@unicesar.edu.co>
2024-12-26 09:37:54 -0500

    Acctividad de VorF y lista desplegable agregada

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,11 @@ import PreguntasProteccion3 from "./pages/Actividades/PreguntasProteccion3.jsx";
 import PreguntasRiesgosMecanicos from "./pages/Actividades/PreguntasRiesgosMecanicos.jsx";
 import PreguntasRiesgosTermicos from "./pages/Actividades/PreguntasRiesgosTermicos.jsx";
 import PreguntasTiposRiesgo from "./pages/Actividades/PreguntasTiposRiesgo.jsx";
-
+import PreguntasVorF from "./pages/Actividades/PreguntasVorF.jsx";
+import ActividadListaDesplegable from "./pages/Actividades/ActividadListaDesplegable.jsx";
 import "./main.css";
+
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
